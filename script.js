15bb5706f94a39aed35d562218a1e86333a6a98c
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-13 22:30:26 -0500

    Added cursor pointer to h3

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-$(document).ready(function()    {
-    var friends = ['Marlon', 'Tes', 'Marvin', 'Asim','Alex']
+$(document).ready(function () {
+    var friends = ['Marlon', 'Tes', 'Marvin', 'Asim', 'Alex']
     var locations = ['New Jersey', 'New York', 'Maine', 'Pennsylvania', 'Connecticut', 'Deleware', 'Minnesota', 'North Carolina', 'Florida', 'Georgia']
-    var weapons = ['Spiked Brush', 'Knife', 'Grenade Launcher', 'Bad Breath', 'Yellow Snowball','Gun', 'Bow & Arrow','Mace', 'NYC Rats', 'Bad', 'Oversized Toothpick', 'Dynamite', 'Baton','Katana', 'My Angry Ex GF', 'Skunk Bomb', 'Magic Wand', 'Large Rock','CrossBow',' Double Sided Pencil' ]
+    var weapons = ['Spiked Brush', 'Knife', 'Grenade Launcher', 'Bad Breath', 'Yellow Snowball', 'Gun', 'Bow & Arrow', 'Mace', 'NYC Rats', 'Bad', 'Oversized Toothpick', 'Dynamite', 'Baton', 'Katana', 'My Angry Ex GF', 'Skunk Bomb', 'Magic Wand', 'Large Rock', 'CrossBow', ' Double Sided Pencil']
 
-        for (var i = 1; i<=100;i++) {
-            var h3 = $('<h3>Accusation' + i + '</h3>' )
-            $('body').append(h3)
-            $(h3).click(getAlert(i))
-        }
-            
-function  getAlert (i)   {
-    var pickFriend = friends[i % 5]
-    var pickLocation = locations[i % 5]
-    var pickWeapon = weapons[i % 5]
-
-    function alertLoop()    {
-        alert("Accusation" + i +  " : I accuse " + pickFriend + " with the " + pickWeapon + " in " + pickLocation + " ! ")
+    for (var i = 1; i <= 100; i++) {
+        var h3 = $('<h3>Accusation' + i + '</h3>')
+        $('body').append(h3)
+        $(h3).click(getAlert(i))
+        $(h3).css({
+            'cursor': 'pointer'
+        });
     }
 
-    return alertLoop
-}
+    function getAlert(i) {
+        var pickFriend = friends[i % 5]
+        var pickLocation = locations[i % 5]
+        var pickWeapon = weapons[i % 5]
+
+        function alertLoop() {
+            alert("Accusation" + i + " : I accuse " + pickFriend + " with the " + pickWeapon + " in " + pickLocation + " ! ")
+        }
+
+        return alertLoop
+    }
 
 
 
-// End of script
+    // End of script
 });
