f237db56b2c3bfe4fb77d4649c6298739a2ad055
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-13 20:22:33 -0500

    Added another friends to friend array

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function()    {
-    var friends = ['Marlon', 'Tes', 'Marvin', 'Asim']
+    var friends = ['Marlon', 'Tes', 'Marvin', 'Asim','Alex']
     var locations = ['New Jersey', 'New York', 'Maine', 'Pennsylvania', 'Connecticut', 'Deleware', 'Minnesota', 'North Carolina', 'Florida', 'Georgia']
     var weapons = ['Spiked Brush', 'Knife', 'Grenade Launcher', 'Bad Breath', 'Yellow Snowball','Gun', 'Bow & Arrow','Mace', 'NYC Rats', 'Bad', 'Oversized Toothpick', 'Dynamite', 'Baton','Katana', 'My Angry Ex GF', 'Skunk Bomb', 'Magic Wand', 'Large Rock','CrossBow',' Double Sided Pencil' ]
-})
+
+
+
+
+
+
+
+
+// End of script
+});
