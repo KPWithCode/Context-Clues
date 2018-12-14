061089d0513b7850b2ad5d641ce5a9c0d60be7cc
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-13 20:21:41 -0500

    Created an array of weapons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function()    {
     var friends = ['Marlon', 'Tes', 'Marvin', 'Asim']
     var locations = ['New Jersey', 'New York', 'Maine', 'Pennsylvania', 'Connecticut', 'Deleware', 'Minnesota', 'North Carolina', 'Florida', 'Georgia']
+    var weapons = ['Spiked Brush', 'Knife', 'Grenade Launcher', 'Bad Breath', 'Yellow Snowball','Gun', 'Bow & Arrow','Mace', 'NYC Rats', 'Bad', 'Oversized Toothpick', 'Dynamite', 'Baton','Katana', 'My Angry Ex GF', 'Skunk Bomb', 'Magic Wand', 'Large Rock','CrossBow',' Double Sided Pencil' ]
 })
