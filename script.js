882b5d74ee0add5b92c455d035ef5827ad0425f6
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-13 20:26:54 -0500

    Added for loop to iterate 100 elements onto the page

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ $(document).ready(function()    {
     var locations = ['New Jersey', 'New York', 'Maine', 'Pennsylvania', 'Connecticut', 'Deleware', 'Minnesota', 'North Carolina', 'Florida', 'Georgia']
     var weapons = ['Spiked Brush', 'Knife', 'Grenade Launcher', 'Bad Breath', 'Yellow Snowball','Gun', 'Bow & Arrow','Mace', 'NYC Rats', 'Bad', 'Oversized Toothpick', 'Dynamite', 'Baton','Katana', 'My Angry Ex GF', 'Skunk Bomb', 'Magic Wand', 'Large Rock','CrossBow',' Double Sided Pencil' ]
 
-
-
+        for (var i = 1; i<=100;i++) {
+            
+        }
 
 
 
