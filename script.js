501e6f9c2f8b484110572cf4c677efcfbad95c2e
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-13 20:10:31 -0500

    Created array for 10 locations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $(document).ready(function()    {
     var friends = ['Marlon', 'Tes', 'Marvin', 'Asim']
-    
+    var locations = ['New Jersey', 'New York', 'Maine', 'Pennsylvania', 'Connecticut', 'Deleware', 'Minnesota', 'North Carolina', 'Florida', 'Georgia']
 })
